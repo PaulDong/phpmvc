32b917947dd89786b902e0f8577b6e3e286fe5fc
Paul Dong <paul@calgah.com>
2022-05-20 14:19:43 -0400

    modify session

--- mvcCore/Session.php
+++ mvcCore/Session.php
@@ -17,15 +17,27 @@ namespace app\mvcCore;
 class Session
 {
   protected const FLASH_KEY = 'flash_messages';
+  protected const WARNING_KEY = 'warning_messages';
+  protected const ERROR_KEY = 'error_messages';
 
   public function __construct()
   {
     session_start();
     $flashMessages = $_SESSION[self::FLASH_KEY] ?? [];
+    $warningMessages = $_SESSION[self::WARNING_KEY] ?? [];
+    $errorMessages = $_SESSION[self::ERROR_KEY] ?? [];
     foreach ($flashMessages as $key => &$flashMessage) {
       $flashMessage['remove'] = true;
     }
+    foreach ($warningMessages as $key => &$warningMessage) {
+      $warningMessage['remove'] = true;
+    }
+    foreach ($errorMessages as $key => &$errorMessage) {
+      $errorMessage['remove'] = true;
+    }
     $_SESSION[self::FLASH_KEY] = $flashMessages;
+    $_SESSION[self::WARNING_KEY] = $warningMessages;
+    $_SESSION[self::ERROR_KEY] = $errorMessages;
   }
 
   public function setFlash($key, $message)
@@ -39,6 +51,32 @@ class Session
   public function getFlash($key)
   {
     return $_SESSION[self::FLASH_KEY][$key]['value'] ?? false;
+  }
+
+  public function setWarning($key, $message)
+  {
+    $_SESSION[self::WARNING_KEY][$key] = [
+      'remove' => false,
+      'value' => $message
+    ];
+  }
+
+  public function getWarning($key)
+  {
+    return $_SESSION[self::WARNING_KEY][$key]['value'] ?? false;
+  }
+
+  public function setError($key, $message)
+  {
+    $_SESSION[self::ERROR_KEY][$key] = [
+      'remove' => false,
+      'value' => $message
+    ];
+  }
+
+  public function getError($key)
+  {
+    return $_SESSION[self::ERROR_KEY][$key]['value'] ?? false;
   }
 
   public function set($key, $value)
@@ -58,10 +96,10 @@ class Session
 
   public function __destruct()
   {
-    $this->removeFlashMessages();
+    $this->removeOldMessages();
   }
 
-  private function removeFlashMessages()
+  private function removeOldMessages()
   {
     $flashMessages = $_SESSION[self::FLASH_KEY] ?? [];
     foreach ($flashMessages as $key => $flashMessage) {
@@ -70,5 +108,21 @@ class Session
       }
     }
     $_SESSION[self::FLASH_KEY] = $flashMessages;
+
+    $warningMessages = $_SESSION[self::WARNING_KEY] ?? [];
+    foreach ($warningMessages as $key => $warningMessage) {
+      if ($warningMessage['remove']) {
+        unset($warningMessages[$key]);
+      }
+    }
+    $_SESSION[self::WARNING_KEY] = $warningMessages;
+
+    $errorMessages = $_SESSION[self::ERROR_KEY] ?? [];
+    foreach ($errorMessages as $key => $errorMessage) {
+      if ($errorMessage['remove']) {
+        unset($errorMessages[$key]);
+      }
+    }
+    $_SESSION[self::ERROR_KEY] = $errorMessages;
   }
 }
